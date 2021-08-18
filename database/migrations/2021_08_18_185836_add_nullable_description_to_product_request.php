bc920fd4c682d4d575945dc912b813d7ec964e2e
himbazayves <mugiranezahimbazayves@gmail.com>
2021-08-18 21:00:03 +0200

    delete some migration

--- database/migrations/2021_08_18_185836_add_nullable_description_to_product_request.php
+++ database/migrations/2021_08_18_185836_add_nullable_description_to_product_request.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddDescriptionToProductRequest2 extends Migration
+class AddNullableDescriptionToProductRequest extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddDescriptionToProductRequest2 extends Migration
     public function up()
     {
         Schema::table('product_requests', function (Blueprint $table) {
-            $table->dropColumn('description');
+            $table->string('description')->nullable();
         });
     }
 
