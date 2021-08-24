250bb8d67c61000d1e608b6d956f7cf7c725661d
himbazayves <mugiranezahimbazayves@gmail.com>
2021-08-24 18:57:51 +0200

    resolving errors

--- resources/views/productRequest/index.blade.php
+++ resources/views/productRequest/index.blade.php
@@ -134,7 +134,7 @@
 
                                     @endphp --}}
 
-                                  {{$requestedTo->name}}
+                                  {{$requestedTo}}
                                 </td>
 
                                 <td>
